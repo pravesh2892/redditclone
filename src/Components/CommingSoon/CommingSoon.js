8b3e759da042536fb6e42c2025cf8639e90adf02
pravesh <pravesh.meena2892@gmail.com>
2023-12-29 11:48:56 +0530

    update

--- src/Components/CommingSoon/CommingSoon.js
+++ src/Components/CommingSoon/CommingSoon.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import not from '../../Assets/not.gif'
 
 function CommingSoon() {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const animateText = (word, index) => {
+      setTimeout(() => {
+        setText(word.substring(0, index + 1));
+        if (index < word.length - 1) {
+          animateText(word, index + 1);
+        }
+      }, 100); 
+    };
+
+    const word = "Coming soon...";
+    animateText(word, 0);
+  }, []);
+
     return (
         <div style={{position:"relative"}}>
           <div
@@ -15,6 +31,7 @@ function CommingSoon() {
             }}
           >
              <img src={not} style={{width:"40%"}}  />
+             <div style={{fontSize:"24px", fontWeight:"bold", animation:"typing 3s steps(40, end)", letterSpacing:"0.15em", marginLeft:"62px"}}>{text}</div>
             <h2 style={{marginTop:"20px", marginLeft:"-45px"}}>We are working on something amazing.</h2>
             <h2 style={{ marginTop: "10px" , marginLeft:"70px"}}>Till then stay tuned</h2>
            
