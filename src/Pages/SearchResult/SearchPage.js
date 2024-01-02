946269a13bf8a7ca92a3a4a7a323343e5fac4beb
pravesh <pravesh.meena2892@gmail.com>
2024-01-02 20:55:50 +0530

    update

--- src/Pages/SearchResult/SearchPage.js
+++ src/Pages/SearchResult/SearchPage.js
@@ -12,7 +12,17 @@ function SearchPage() {
         {search?.data?.length ? (
           search.data.map((fed) => <Feed fed={fed} key={fed._id} />)
         ) : (
-          <p className="not-found-message">No data found</p>
+          <div className="flex mt-md p-md bg-neutral-background-weak" >
+      <img  src="https://www.redditstatic.com/shreddit/assets/telescope-snoo.svg"></img>
+      <div className='text'>
+        <div class="text-neutral-content font-semibold text-16 xs:text-18 mb-xs">
+          Hm... we couldn’t find any results 
+        </div>
+        <div class="text-second-line">
+          Double-check your spelling or try different keywords to adjust your search
+        </div>
+      </div>
+  </div>
         )}
       </div>
     </>
